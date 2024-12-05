7b60f222ce3b879a7787363c46b4b29bc7184795
MuhammedKasujja <al.kasmud.2@gmail.com>
2024-12-05 17:20:35 +0300

    feat: added portfolio sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { About } from "./components/about/About";
+import { Experience } from "./components/experiense/Experience";
+import { Projects } from "./components/projects/Project";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex flex-col gap-5 justify-center w-screen p-5">
+      <About />
+      <Experience />
+      <Projects />
+    </div>
+  );
 }
