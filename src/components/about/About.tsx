ec41c890ccbb7b52ad4607419996245524f4d995
MuhammedKasujja <al.kasmud.2@gmail.com>
2024-12-23 10:36:24 -0800

    feat: display profile image

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -5,8 +5,8 @@ export function About() {
   return (
     <div className="">
       <Avatar className="w-72 h-[400px] rounded-md">
-        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-        <AvatarFallback>CN</AvatarFallback>
+        <AvatarImage src="src/assets/20240516_090002.jpg" alt="@shadcn" />
+        <AvatarFallback className="rounded-md">KM</AvatarFallback>
       </Avatar>
       <h1 className="text-3xl font-bold">{greeting.fullName}</h1>
       <a href={greeting.githubLink} target="_blank">
