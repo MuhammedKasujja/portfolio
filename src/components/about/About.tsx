8fed9c1a6d1f393e4aac1b357be6c9dfbca04719
MuhammedKasujja <al.kasmud.2@gmail.com>
2024-12-23 11:02:41 -0800

    fix: loading profile image
    feat: change site icon

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,11 +1,12 @@
 import { greeting } from "../../data";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import profileImage from "@/assets/20240516_090002.jpg"
 
 export function About() {
   return (
     <div className="">
       <Avatar className="w-72 h-[400px] rounded-md">
-        <AvatarImage src="src/assets/20240516_090002.jpg" alt="@shadcn" />
+        <AvatarImage src={profileImage} alt="@km" />
         <AvatarFallback className="rounded-md">KM</AvatarFallback>
       </Avatar>
       <h1 className="text-3xl font-bold">{greeting.fullName}</h1>
