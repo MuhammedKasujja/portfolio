f8ca6b3db7f1f059a3aec4ded4a463519f1d995c
MuhammedKasujja <al.kasmud.2@gmail.com>
2024-12-23 11:25:08 -0800

    refactor: loading projects data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -10,7 +10,7 @@ export const projectsHeader = {
 export const projects: IProject[] = [
   {
     id: "0",
-    name: "Keep Calm and Carry On",
+    name: "Metadent",
     url: "https://github.com/harikanani/Keep-Calm-and-Carry-On.github.io",
     description:
       "This platform enables users to practice Augmented Reality meditation, play stress burster games, track task completion and engage in motivational exercises to cope with stress and anxiety at work.  ",
@@ -48,7 +48,7 @@ export const projects: IProject[] = [
   },
   {
     id: "1",
-    name: "YOG4LIFE SOCIAL APP",
+    name: "Smartrides",
     url: "https://github.com/kevalvavaliya/Yog4Life",
     description:
       "An app for helping people their healthy life. Yog4Life Has a feed feature which is help to get information from the folks, Yog4Life has an anonymous chat room to get connected with the world.",
@@ -77,7 +77,7 @@ export const projects: IProject[] = [
   },
   {
     id: "2",
-    name: "Adoptisity",
+    name: "Lenderly",
     url: "https://github.com/kevalvavaliya/Adoptisity",
     description:
       "Our mission is to spread awareness about the benefits of adoption and to support LGBT families so that everyone has the chance to have a loving home environment.",
@@ -119,7 +119,7 @@ export const projects: IProject[] = [
   },
   {
     id: "3",
-    name: "FinSplit",
+    name: "Kaltech Stores",
     url: "https://github.com/rajmahadev8/Fin-Split",
     description:
       "Split your bills with your friends without any hassle. Manage Group Expenses, Track your expenses, and much more.",
@@ -148,7 +148,7 @@ export const projects: IProject[] = [
   },
   {
     id: "4",
-    name: "Emosic",
+    name: "FundsTrans",
     url: "https://github.com/kevalvavaliya/Emosic",
     description:
       "An app that plays music according to your mood. Created a playlist management UI and used SpotifyAPI to control player client based on mood detection using ml model.",
@@ -181,40 +181,35 @@ export const projects: IProject[] = [
   },
   {
     id: "5",
-    name: "Fileblok",
-    url: "https://github.com/harikanani/Fileblok",
+    name: "AutoDoc",
+    url: "https://github.com/harikanani/e-commerce-frontend",
     description:
-      "Keep your projects moving with seamless transfer experice. FileBlok is a file transfer app that allows you to transfer files from one device to another without any hassle. We built this app using Flutter, Node.js and StoryBlok.",
+      "An E-commerce website built using MERN[Mongo, Express, React, Node] stack and we have also added stripe payment gateway integration [in test mode].",
     languages: [
       {
-        name: "Flutter",
-        iconifyClass: "logos-flutter",
+        name: "ReactJS",
+        iconifyClass: "logos-react",
       },
       {
-        name: "NodeJs",
+        name: "NodeJS",
         iconifyClass: "logos-nodejs",
       },
       {
-        name: "Storyblok",
-        iconifyClass: "logos-storyblok-icon",
+        name: "MedusaJS",
+        iconifyClass: "logos-medusa-icon",
       },
     ],
     links: [
       {
         name: "Github",
-        url: "https://github.com/harikanani/Fileblok",
+        url: "https://github.com/harikanani/e-commerce-frontend",
         iconifyClass: "ri:github-fill",
-      },
-      {
-        name: "Devpost",
-        url: "https://devpost.com/software/fileblok",
-        iconifyClass: "fluent:window-dev-edit-20-filled",
       },
     ],
   },
   {
     id: "6",
-    name: "BAUDDIK-GEEKS PORTFOLIO",
+    name: "Pro AIConnect",
     url: "https://github.com/Bauddhik-Geeks/Bauddhik-Geeks.github.io",
     description:
       "Awesome Community Portfolio website for Bauddhik-Geeks community",
@@ -247,7 +242,7 @@ export const projects: IProject[] = [
   },
   {
     id: "7",
-    name: "Votechain",
+    name: "Offline-Sync Module",
     url: "https://github.com/abhigoyani/votechain",
     description:
       "Awesome Community Portfolio website for Bauddhik-Geeks community",
@@ -283,109 +278,15 @@ export const projects: IProject[] = [
   },
   {
     id: "8",
-    name: "Swag Store",
-    url: "https://github.com/harikanani/e-commerce-frontend",
+    name: "Inputi",
+    url: "https://github.com/rajmahadev8/Fin-Split",
     description:
-      "An E-commerce website built using MERN[Mongo, Express, React, Node] stack and we have also added stripe payment gateway integration [in test mode].",
+      "Split your bills with your friends without any hassle. Manage Group Expenses, Track your expenses, and much more.",
     languages: [
       {
         name: "ReactJS",
         iconifyClass: "logos-react",
       },
-      {
-        name: "NodeJS",
-        iconifyClass: "logos-nodejs",
-      },
-      {
-        name: "MedusaJS",
-        iconifyClass: "logos-medusa-icon",
-      },
-    ],
-    links: [
-      {
-        name: "Github",
-        url: "https://github.com/harikanani/e-commerce-frontend",
-        iconifyClass: "ri:github-fill",
-      },
-    ],
-  },
-  {
-    id: "9",
-    name: "node-blockchain",
-    url: "https://github.com/harikanani/node-blockchain",
-    description:
-      "A simple blockchain and cryptocurrency wallet implemented in Node.js and TypeScript (for learning purposes).",
-    languages: [
-      {
-        name: "NodeJS",
-        iconifyClass: "logos-nodejs",
-      },
-      {
-        name: "TypeScript",
-        iconifyClass: "skill-icons:typescript",
-      },
-    ],
-    links: [
-      {
-        name: "Github",
-        url: "https://github.com/harikanani/node-blockchain",
-        iconifyClass: "ri:github-fill",
-      },
-    ],
-  },
-  {
-    id: "10",
-    name: "Know Me Well",
-    url: "https://github.com/harikanani/personal-portfolio",
-    description:
-      "A simple command line interface based quiz app to know more about me :).",
-    languages: [
-      {
-        name: "JavaScript",
-        iconifyClass: "logos-javascript",
-      },
-      {
-        name: "NodeJS",
-        iconifyClass: "logos-nodejs",
-      },
-      {
-        name: "Repl.it",
-        iconifyClass: "logos-replit-icon",
-      },
-    ],
-    links: [
-      {
-        name: "Github",
-        url: "https://github.com/harikanani/personal-portfolio",
-        iconifyClass: "ri:github-fill",
-      },
-    ],
-  },
-  {
-    id: "11",
-    name: "Bloggify",
-    url: "https://github.com/harikanani/Bloggify",
-    description: "A Simple REST API for Blog Application.",
-    languages: [
-      {
-        name: "NodeJS",
-        iconifyClass: "logos-nodejs",
-      },
-    ],
-    links: [
-      {
-        name: "Github",
-        url: "https://github.com/harikanani/Bloggify",
-        iconifyClass: "ri:github-fill",
-      },
-    ],
-  },
-  {
-    id: "12",
-    name: "Stargazzers",
-    url: "https://github.com/harikanani/stargazers",
-    description: "An Unofficial API for GitHub Repo Stars Count",
-    languages: [
       {
         name: "NodeJs",
         iconifyClass: "logos-nodejs",
@@ -394,8 +295,13 @@ export const projects: IProject[] = [
     links: [
       {
         name: "Github",
-        url: "https://github.com/harikanani/stargazers",
+        url: "https://github.com/rajmahadev8/Fin-Split",
         iconifyClass: "ri:github-fill",
+      },
+      {
+        name: "Devpost",
+        url: "https://devpost.com/software/finsplit",
+        iconifyClass: "fluent:window-dev-edit-20-filled",
       },
     ],
   },
@@ -432,237 +338,4 @@ export const projects: IProject[] = [
       },
     ],
   },
-  // {
-  //   id: "6",
-  //   name: "Automate Attendace",
-  //   url: "https://github.com/harikanani/Node_Python",
-  //   description: "Automation of Online Attendance using node js and python",
-  //   languages: [
-  //     {
-  //       name: "JavaScript",
-  //       iconifyClass: "logos-javascript",
-  //     },
-  //     {
-  //       name: "Python",
-  //       iconifyClass: "logos-python",
-  //     },
-  //   ],
-  //   links: [
-  //     {
-  //       name: "Github",
-  //       url: "",
-  //       iconifyClass: "ri:github-fill",
-  //     },
-  //     {
-  //       name: "Devfolio",
-  //       url: "",
-  //       iconifyClass: "ri:github-fill",
-  //     },
-  //   ],
-  // },
-  // {
-  //   id: "7",
-  //   name: "Automate Discord Bot",
-  //   url: "https://github.com/harikanani/AutomateDiscordBot",
-  //   description:
-  //     "A Discord Bot to send Automatic messages to serer channel in regular time difference.",
-  //   languages: [
-  //     {
-  //       name: "Python",
-  //       iconifyClass: "logos-python",
-  //     },
-  //     {
-  //       name: "Python Selenium",
-  //       iconifyClass: "logos-selenium",
-  //     },
-  //     {
-  //       name: "Chromium Browser",
-  //       iconifyClass: "openmoji-chromium",
-  //     },
-  //   ],
-  //   links: [
-  //     {
-  //       name: "Github",
-  //       url: "",
-  //       iconifyClass: "ri:github-fill",
-  //     },
-  //     {
-  //       name: "Devfolio",
-  //       url: "",
-  //       iconifyClass: "ri:github-fill",
-  //     },
-  //   ],
-  // },
-  // {
-  //   id: "8",
-  //   name: "Flask Blog",
-  //   url: "https://github.com/harikanani/flask_blog",
-  //   description: "A Simple Blog Web Application made using Flask Framework",
-  //   languages: [
-  //     {
-  //       name: "Python",
-  //       iconifyClass: "logos-python",
-  //     },
-  //     {
-  //       name: "Flask",
-  //       iconifyClass: "cib-flask",
-  //     },
-  //     {
-  //       name: "HTML5",
-  //       iconifyClass: "vscode-icons:file-type-html",
-  //     },
-  //     {
-  //       name: "CSS3",
-  //       iconifyClass: "vscode-icons:file-type-css",
-  //     },
-  //   ],
-  //   links: [
-  //     {
-  //       name: "Github",
-  //       url: "",
-  //       iconifyClass: "ri:github-fill",
-  //     },
-  //     {
-  //       name: "Devfolio",
-  //       url: "",
-  //       iconifyClass: "ri:github-fill",
-  //     },
-  //   ],
-  // },
-  // {
-  //   id: "9",
-  //   name: "Netflix top series",
-  //   url: "https://github.com/harikanani/netflix-top-series",
-  //   description: "List of Top Netflix Series which is deployed to vercel.",
-  //   languages: [
-  //     {
-  //       name: "JavaScript",
-  //       iconifyClass: "logos-javascript",
-  //     },
-  //     {
-  //       name: "ReactJS",
-  //       iconifyClass: "logos-react",
-  //     },
-  //     {
-  //       name: "HTML5",
-  //       iconifyClass: "logos-html-5",
-  //     },
-  //     {
-  //       name: "CSS3",
-  //       iconifyClass: "logos-css-3",
-  //     },
-  //   ],
-  //   links: [
-  //     {
-  //       name: "Github",
-  //       url: "",
-  //       iconifyClass: "ri:github-fill",
-  //     },
-  //     {
-  //       name: "Devfolio",
-  //       url: "",
-  //       iconifyClass: "ri:github-fill",
-  //     },
-  //   ],
-  // },
-  {
-    id: "10",
-    name: "COVID-19 Tracker",
-    url: "https://github.com/harikanani/Covid19TrackerReact",
-    description:
-      "Simple Covid-19 Tracker made using React and deployed to Vercel.",
-    languages: [
-      {
-        name: "JavaScript",
-        iconifyClass: "logos-javascript",
-      },
-      {
-        name: "ReactJS",
-        iconifyClass: "logos-react",
-      },
-      {
-        name: "HTML5",
-        iconifyClass: "logos-html-5",
-      },
-      {
-        name: "CSS3",
-        iconifyClass: "logos-css-3",
-      },
-    ],
-    links: [
-      {
-        name: "Github",
-        url: "https://github.com/harikanani/Covid19TrackerReact",
-        iconifyClass: "ri:github-fill",
-      },
-    ],
-  },
-  // {
-  //   id: "11",
-  //   name: "Food Order Static Website",
-  //   url: "https://github.com/harikanani/food-order-website",
-  //   description:
-  //     "A simple static website related to food restaurants service. this is reasponsive as well.",
-  //   languages: [
-  //     {
-  //       name: "HTML5",
-  //       iconifyClass: "logos-html-5",
-  //     },
-  //     {
-  //       name: "CSS3",
-  //       iconifyClass: "logos-css-3",
-  //     },
-  //     {
-  //       name: "JavaScript",
-  //       iconifyClass: "logos-javascript",
-  //     },
-  //     {
-  //       name: "PHP",
-  //       iconifyClass: "logos-php",
-  //     },
-  //   ],
-  //   links: [
-  //     {
-  //       name: "Github",
-  //       url: "",
-  //       iconifyClass: "ri:github-fill",
-  //     },
-  //     {
-  //       name: "Devfolio",
-  //       url: "",
-  //       iconifyClass: "ri:github-fill",
-  //     },
-  //   ],
-  // },
-  // {
-  //   id: "12",
-  //   name: "NFT Launchpad",
-  //   url: "https://deliquescent-cents.000webhostapp.com/",
-  //   description: "NFT Launchpad crypto site portfolio",
-  //   languages: [
-  //     {
-  //       name: "HTML5",
-  //       iconifyClass: "logos-html-5",
-  //     },
-  //     {
-  //       name: "CSS3",
-  //       iconifyClass: "logos-css-3",
-  //     },
-  //     {
-  //       name: "JavaScript",
-  //       iconifyClass: "logos-javascript",
-  //     },
-  //     {
-  //       name: "ReactJS",
-  //       iconifyClass: "logos-react",
-  //     },
-  //   ],
-  //   links: [
-  //     {
-  //       name: "Github",
-  //       url: "https://github.com/harikanani/nft-launchpad",
-  //       iconifyClass: "ri:github-fill",
-  //     },
-  //   ],
-  // },
 ];
