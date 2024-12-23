14402e0e53d34bce2fa23ba86782c1111eeda7b9
MuhammedKasujja <al.kasmud.2@gmail.com>
2024-12-23 14:04:14 -0800

    refactor: ExperienceCard component

--- src/components/experiense/components/ExperienceCard.tsx
+++ src/components/experiense/components/ExperienceCard.tsx
@@ -14,16 +14,18 @@ export function ExperienceCard({ experience }: Prop) {
   return (
     <Accordion type="single" collapsible className="">
       <AccordionItem value={experience.company}>
-        <AccordionTrigger className="flex flex-col">
-          <div className="flex flex-row items-start justify-between w-full gap-5">
-            <div className="font-semibold">{experience.title}</div>
-            <div className="text-sm">
-              {experience.duration.startDate} - {experience.duration.endDate}
+        <AccordionTrigger>
+          <div className="flex flex-col w-full gap-2 pr-5">
+            <div className="flex flex-row items-start justify-between w-full gap-2 md:gap-5">
+              <div className="font-semibold">{experience.title}</div>
+              <div className="text-sm">
+                {experience.duration.startDate} - {experience.duration.endDate}
+              </div>
             </div>
-          </div>
-          <div className="flex flex-row items-baseline justify-between w-full gap-5">
-            <div>{experience.company}</div>
-            <div>{experience.location}</div>
+            <div className="flex flex-row items-baseline justify-between w-full gap-2 md:gap-5">
+              <div>{experience.company}</div>
+              <div>{experience.location}</div>
+            </div>
           </div>
         </AccordionTrigger>
         <AccordionContent>{experience.description}</AccordionContent>
--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -16,14 +16,8 @@ export function ThemeToggle() {
 
   return (
     <Button variant="outline" size="icon" onClick={handleToggleTheme}>
-      <Sun
-        className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-        onClick={() => setTheme("light")}
-      />
-      <Moon
-        className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-        onClick={() => setTheme("dark")}
-      />
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
