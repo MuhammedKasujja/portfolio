60b82258967a8e087e4e0c997c8f4c03a029b881
MuhammedKasujja <al.kasmud.2@gmail.com>
2025-03-23 10:51:54 -0700

    refactor: experience layout using cards

--- src/components/experiense/components/ExperienceCard.tsx
+++ src/components/experiense/components/ExperienceCard.tsx
@@ -1,9 +1,4 @@
-// import {
-//   Accordion,
-//   AccordionContent,
-//   AccordionItem,
-//   AccordionTrigger,
-// } from "@/components/ui/accordion";
+import { Card, CardDescription, CardHeader } from "@/components/ui/card";
 import { IExperience } from "../../../types";
 
 type Prop = {
@@ -12,41 +7,27 @@ type Prop = {
 
 export function ExperienceCard({ experience }: Prop) {
   return (
-    <div className="p-4 space-y-2 border rounded-md">
-        <div>
-          <div className="flex flex-col w-full gap-2 pr-5">
-            <div className="flex flex-row items-start justify-between w-full gap-2 md:gap-5">
-              <div className="font-semibold">{experience.title}</div>
-              <div className="text-sm">
-                {experience.duration.startDate} - {experience.duration.endDate}
+    <Card className="w-full">
+      <CardHeader>
+        <CardDescription className="space-y-2">
+          <div>
+            <div className="flex flex-col w-full gap-2 pr-5">
+              <div className="flex flex-row items-start justify-between w-full gap-2 md:gap-5">
+                <div className="font-semibold">{experience.title}</div>
+                <div className="text-sm">
+                  {experience.duration.startDate} -{" "}
+                  {experience.duration.endDate}
+                </div>
+              </div>
+              <div className="flex flex-row items-baseline justify-between w-full gap-2 md:gap-5">
+                <div className="text-sm">{experience.company}</div>
+                <div className="text-sm">{experience.location}</div>
               </div>
             </div>
-            <div className="flex flex-row items-baseline justify-between w-full gap-2 md:gap-5">
-              <div className="text-sm">{experience.company}</div>
-              <div className="text-sm">{experience.location}</div>
-            </div>
           </div>
-        </div>
-        <div className="pr-5">{experience.description}</div>
-    </div>
-    // <Accordion type="single" collapsible className="">
-    //   <AccordionItem value={experience.company}>
-    //     <AccordionTrigger>
-    //       <div className="flex flex-col w-full gap-2 pr-5">
-    //         <div className="flex flex-row items-start justify-between w-full gap-2 md:gap-5">
-    //           <div className="font-semibold">{experience.title}</div>
-    //           <div className="text-sm">
-    //             {experience.duration.startDate} - {experience.duration.endDate}
-    //           </div>
-    //         </div>
-    //         <div className="flex flex-row items-baseline justify-between w-full gap-2 md:gap-5">
-    //           <div>{experience.company}</div>
-    //           <div>{experience.location}</div>
-    //         </div>
-    //       </div>
-    //     </AccordionTrigger>
-    //     <AccordionContent>{experience.description}</AccordionContent>
-    //   </AccordionItem>
-    // </Accordion>
+          <div className="pr-5">{experience.description}</div>
+        </CardDescription>
+      </CardHeader>
+    </Card>
   );
 }
