3675dfdb313eea06862e52f76326191e890a6376
MuhammedKasujja <al.kasmud.2@gmail.com>
2025-03-23 11:48:12 -0700

    refactor: projects layout

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -4,8 +4,8 @@ import { ProjectItem } from "./components/ProjectItem";
 export function Projects() {
   return (
     <div className="space-y-5" id="projects">
-      <h1 className="font-bold text-2xl">Projects</h1>
-      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+      <h1 className="text-2xl font-bold">Projects</h1>
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
         {projects.map((project) => (
           <ProjectItem project={project} key={project.name} />
         ))}
